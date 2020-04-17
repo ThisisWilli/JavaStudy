4faf76ed024217bbc91a781144e872e3eb90ebcf
ThisisWilli <thisiswilli@163.com>
2020-04-17 10:47:53 +0800

    贪心算法跳台阶

---
+++ src/main/java/com/coding/leetcode/array/canJump55/Solution.java
@@ -0,0 +1,49 @@
+package com.coding.leetcode.array.canJump55;
+
+/**
+ * @program: JavaStudy
+ * @description:
+ * @author: Hoodie_Willi
+ * @create: 2020-04-17 09:27
+ *
+ * 给定一个非负整数数组，你最初位于数组的第一个位置。
+ *
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * 判断你是否能够到达最后一个位置。
+ *
+ * 示例 1:
+ *       [0,1,2,3,4]
+ * 输入: [2,3,1,1,4]
+ * 输出: true
+ * 解释: 我们可以先跳 1 步，从位置 0 到达 位置 1, 然后再从位置 1 跳 3 步到达最后一个位置。
+ * 示例 2:
+ *       [0,1,2,3,4]
+ * 输入: [3,2,1,0,4]
+ * 输出: false
+ * 解释: 无论怎样，你总会到达索引为 3 的位置。但该位置的最大跳跃长度是 0 ， 所以你永远不可能到达最后一个位置。
+ *
+ **/
+
+public class Solution {
+    public boolean canJump(int[] nums) {
+        /*
+         * 从i开始跳，维护一个能跳到的最远max值，
+         * */
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (max < i + nums[i] && max >= i){
+                max = i + nums[i];
+            }
+            if (max >= nums.length - 1){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        boolean b = new Solution().canJump(new int[]{3, 2, 1, 0, 4});
+        System.out.println(b);
+    }
+}
