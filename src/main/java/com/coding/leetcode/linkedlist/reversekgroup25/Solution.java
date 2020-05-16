d225e7983e9f18e77826a9951f23f948672f7c7e
ThisisWilli <thisiswilli@163.com>
2020-05-16 10:34:32 +0800

    头插法反转k个链表，要记录最开始反转的那个节点

---
+++ src/main/java/com/coding/leetcode/linkedlist/reversekgroup25/Solution.java
@@ -0,0 +1,67 @@
+package com.coding.leetcode.linkedlist.reversekgroup25;
+
+/**
+ * \* project: JavaStudy
+ * \* package: com.coding.leetcode.linkedlist.reversekgroup25
+ * \* author: Willi Wei
+ * \* date: 2020-05-16 08:52:21
+ * \* description:
+ *
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ * k 是一个正整数，它的值小于或等于链表的长度。
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * 示例：
+ *
+ * 给你这个链表：1->2->3->4->5
+ *
+ * 当 k = 2 时，应当返回: 2->1->4->3->5
+ *
+ * 当 k = 3 时，应当返回: 3->2->1->4->5
+ */
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode end = dummy;
+
+        while (end.next != null){
+            // 到达一个要切分的点
+            for (int i = 0; i < k && end != null; i++){
+                end = end.next;
+            }
+            if (end == null){
+                break;
+            }
+            // 记录开始节点
+            ListNode start = pre.next;
+            // 记录反转之后要连接的下一个节点
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+            end = pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head){
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
