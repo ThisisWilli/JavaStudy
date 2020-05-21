0c6b8e73328dd11d0b6b36e2fc8f80ffce4e81e6
ThisisWilli <thisiswilli@163.com>
2020-05-21 09:47:06 +0800

    判断字符串中的最长回文子串

---
+++ src/main/java/com/coding/leetcode/string/findthelongestsubstring1371/Solution.java
@@ -0,0 +1,62 @@
+package com.coding.leetcode.string.findthelongestsubstring1371;
+
+import java.util.*;
+
+/**
+ * \* project: JavaStudy
+ * \* package: com.coding.leetcode.string.findthelongestsubstring1371
+ * \* author: Willi Wei
+ * \* date: 2020-05-20 08:45:16
+ * \* description:
+ * 给你一个字符串 s ，请你返回满足以下条件的最长子字符串的长度：每个元音字母，即 'a'，'e'，'i'，'o'，'u' ，在子字符串中都恰好出现了偶数次。
+ * 示例 1：
+ *
+ * 输入：s = "eleetminicoworoep"
+ * 输出：13
+ * 解释：最长子字符串是 "leetminicowor" ，它包含 e，i，o 各 2 个，以及 0 个 a，u 。
+ * 示例 2：
+ *
+ * 输入：s = "leetcodeisgreat"
+ * 输出：5
+ * 解释：最长子字符串是 "leetc" ，其中包含 2 个 e 。
+ * 示例 3：
+ *
+ * 输入：s = "bcbcbc"
+ * 输出：6
+ * 解释：这个示例中，字符串 "bcbcbc" 本身就是最长的，因为所有的元音 a，e，i，o，u 都出现了 0 次。
+ * \
+ */
+public class Solution {
+    public int findTheLongestSubstring(String s) {
+        int n = s.length();
+        int[] pos = new int[1 << 5];
+        Arrays.fill(pos, -1);
+        int ans = 0, status = 0;
+        pos[0] = 0;
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch == 'a') {
+                status ^= (1 << 0);
+            } else if (ch == 'e') {
+                status ^= (1 << 1);
+            } else if (ch == 'i') {
+                status ^= (1 << 2);
+            } else if (ch == 'o') {
+                status ^= (1 << 3);
+            } else if (ch == 'u') {
+                status ^= (1 << 4);
+            }
+            if (pos[status] >= 0) {
+                ans = Math.max(ans, i + 1 - pos[status]);
+            } else {
+                pos[status] = i + 1;
+            }
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().findTheLongestSubstring("leetcodeisgreat"));
+    }
+}
---
+++ src/main/java/com/coding/leetcode/string/longestpalindrome5/Solution.java
@@ -0,0 +1,48 @@
+package com.coding.leetcode.string.longestpalindrome5;
+
+/**
+ * \* project: JavaStudy
+ * \* package: com.coding.leetcode.string.longestpalindrome5
+ * \* author: Willi Wei
+ * \* date: 2020-05-21 08:48:18
+ * \* description:
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 示例 1：
+ *
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ * 示例 2：
+ *
+ * 输入: "cbbd"
+ * 输出: "bb"
+ * \
+ */
+public class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        String ans = "";
+        for (int l = 0; l < n; l++) {
+            for (int i = 0; i + l < n; i++){
+                int j = i + l;
+                if (i == j){
+                    dp[i][j] = true;
+                }else if (l == 1){
+                    dp[i][j] = (s.charAt(i) == s.charAt(j));
+                }else {
+                    dp[i][j] = dp[i + 1][j - 1] && (s.charAt(i) == s.charAt(j));
+                }
+                if (dp[i][j] && l + 1> ans.length()){
+                    ans = s.substring(i, j + 1);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().longestPalindrome("b"));
+    }
+}
